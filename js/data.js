29ba4994b9ccb2281480bd6d4a8ebf19e3039c5a
Ishan Lahiru <ishanlahiru2002@gmail.com>
2023-10-11 15:00:26 +0530

    hero section modified

--- js/data.js
+++ js/data.js
@@ -1,4 +1,5 @@
-const token = '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';
+const token = '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';
+let theData = {};
 
 fetch('https://api.github.com/repos/IshanLahiru/ishanlahiru.github.io/contents/data.md', {
   method: 'GET',
@@ -9,12 +10,12 @@ fetch('https://api.github.com/repos/IshanLahiru/ishanlahiru.github.io/contents/d
   .then(response => response.json())
   .then(data => {
     // Handle the data from the API response
-    let siteName = document.getElementsByClassName("navbar-brand");
+    let siteName = document.getElementsByClassName("introduction");
     console.log(siteName);
     console.log(data);
     let parsedData = JSON.parse(atob(data.content));
     console.log(parsedData[0]);
-    let theData = parsedData[0];
+    theData = parsedData[0];
     siteName.item(0).innerHTML = `${theData.heroSection.fname} ${theData.heroSection.lname}`;
   })
   .catch(error => {
