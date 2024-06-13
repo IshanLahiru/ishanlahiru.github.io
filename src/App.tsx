584e5306f86757db56b3ed29c0407a6f008b0790
IshanLahiru <ishanlahiru2002@gmail.com>
2024-06-13 20:58:48 +0530

    added the react router and setting up the pages setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Navigate replace to="home"/>}/>
+        <Route path="/" element={<div/>}></Route>
+          <Route path="home" element={<div>the home</div>}/>
+          <Route path="about" element={<div>the about section</div>}/>
+          <Route path="projects" element={<div>the projects section</div>}/>
+          <Route path="certificates" element={<div>the certificates section</div>}/>
+          <Route path="contact" element={<div>the contact section</div>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
