2ea31c86c9b3bcef91846505e0a5465e89e4e43b
IshanLahiru <ishanlahiru2002@gmail.com>
2024-09-22 20:24:10 +0530

    feat: Add Recommendations page component

---
+++ src/pages/recommendations/recommendations.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Recommendations: React.FC = () => {
+    return (
+        <div>
+            <h1>Recommendations Page</h1>
+            <p>This is the recommendation page content.</p>
+        </div>
+    );
+};
+
+export default Recommendations;
