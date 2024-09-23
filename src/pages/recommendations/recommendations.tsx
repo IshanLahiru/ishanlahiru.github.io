fd841668a5832e0bcb44afa5a5da728e81af7ad8
IshanLahiru <ishanlahiru2002@gmail.com>
2024-09-23 23:52:25 +0530

    Refactor indentation in Recommendations component

--- src/pages/recommendations/recommendations.tsx
+++ src/pages/recommendations/recommendations.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 const Recommendations: React.FC = () => {
-    return (
-        <div>
-            <h1>Recommendations Page</h1>
-            <p>This is the recommendation page content.</p>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Recommendations Page</h1>
+      <p>This is the recommendation page content.</p>
+    </div>
+  );
 };
 
 export default Recommendations;
