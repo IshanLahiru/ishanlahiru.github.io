f9226698964b86421410464414bc6b5967215643
IshanLahiru <ishanlahiru2002@gmail.com>
2024-09-23 23:51:57 +0530

    Refactor 404.tsx to fix indentation

--- src/pages/404/404.tsx
+++ src/pages/404/404.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const InvalidSection: React.FC = () => {
-    return (
-        <div>
-            <h1>404 Page</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>404 Page</h1>
+    </div>
+  );
 };
 
 export default InvalidSection;
