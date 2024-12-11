4b4a8d3b1848af65c5cd4128738cc4ef3c57a4be
IshanLahiru <ishanlahiru2002@gmail.com>
2024-12-12 05:29:04 +0530

    Refactor App component: replace static content with Router component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import Router from './routes/router';
 
 const App: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <header className="text-center">
-        <h1 className="text-4xl font-bold text-blue-500">Welcome to</h1>
-        <h2 className="text-5xl font-extrabold text-gray-800 mt-2">ishanlahiru.me</h2>
-      </header>
-    </div>
+    <Router />
+    // <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    //   <header className="text-center">
+    //     <h1 className="text-4xl font-bold text-blue-500">Welcome to</h1>
+    //     <h2 className="text-5xl font-extrabold text-gray-800 mt-2">ishanlahiru.me</h2>
+    //   </header>
+    // </div>
   );
 };
 
