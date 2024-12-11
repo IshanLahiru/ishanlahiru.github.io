fd7200703951d3fca614b690d68d31fa25d356ca
IshanLahiru <ishanlahiru2002@gmail.com>
2024-12-12 05:06:19 +0530

    Refactor Tailwind CSS configuration for improved readability

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html", "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {},
     screens: {
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px'
     }
   },
-  plugins: [],
-}
-
+  plugins: []
+};
