c8898100aa5d8c495aeed69842db8db8b445e13f
IshanLahiru <ishanlahiru2002@gmail.com>
2025-01-10 05:34:56 +0530

    Refactor HomePage component to use BaseContainer and NavigationBar

--- src/routes/home/homePage.tsx
+++ src/routes/home/homePage.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
+import BaseContainer from '../../components/base-container/baseContainer';
+import NavigationBar from '../../components/header/header';
 
 const HomePage: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+    <BaseContainer>
+      <NavigationBar />
       <header className="text-center">
         <h1 className="text-4xl font-bold text-blue-500">Welcome to</h1>
         <h2 className="text-5xl font-extrabold text-gray-800 mt-2">ishanlahiru.me</h2>
       </header>
-    </div>
+    </BaseContainer>
   );
 };
 
