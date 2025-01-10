199f1d41af0832ad30c45b32b92c455b77c44d8e
IshanLahiru <ishanlahiru2002@gmail.com>
2025-01-10 23:21:27 +0530

    Adjust BaseContainer layout for improved side panel responsiveness

--- src/components/base-container/baseContainer.tsx
+++ src/components/base-container/baseContainer.tsx
@@ -5,9 +5,9 @@ interface BaseContainerProps {
 const BaseContainer: React.FC<BaseContainerProps> = ({ children }) => {
   return (
     <div className={`flex flex-row h-screen w-screen  dark:bg-stone-900 bg-slate-100`}>
-      <div className={`hidden sm:block basis-1/4 md:basis-1/10 dark:bg-stone-900 bg-slate-100`} />
+      <div className={`hidden sm:block basis-1/2 md:basis-1/8 dark:bg-stone-900 bg-slate-100`} />
       <div className="grow h-screen mx-5">{children}</div>
-      <div className={`hidden sm:block basis-1/4 md:basis-1/10 dark:bg-stone-900 bg-slate-100`} />
+      <div className={`hidden sm:block basis-1/2 md:basis-1/8 dark:bg-stone-900 bg-slate-100`} />
     </div>
   );
 };
