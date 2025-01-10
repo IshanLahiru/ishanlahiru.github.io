81d68c80cf88e3de71696d2080e35587ba57068f
IshanLahiru <ishanlahiru2002@gmail.com>
2025-01-10 22:07:20 +0530

    Refactor BaseContainer layout for improved responsiveness and styling

--- src/components/base-container/baseContainer.tsx
+++ src/components/base-container/baseContainer.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
-
 interface BaseContainerProps {
   children: React.ReactNode;
 }
 
 const BaseContainer: React.FC<BaseContainerProps> = ({ children }) => {
   return (
-    <div className="flex flex-row h-screen w-screen bg-slate-200 dark:bg-stone-900 ">
-      <div className="hidden sm:block basis-1/3 md:basis-1/10 bg-slate-100 dark:bg-stone-900" />
-      <div className="grow w-screen h-screen">{children}</div>
-      <div className="hidden sm:block basis-1/3 md:basis-1/10 bg-slate-100 dark:bg-stone-900" />
+    <div className={`flex flex-row h-screen w-screen  dark:bg-stone-900 bg-slate-100`}>
+      <div className={`hidden sm:block basis-1/4 md:basis-1/10 dark:bg-stone-900 bg-slate-100`} />
+      <div className="grow h-screen mx-5">{children}</div>
+      <div className={`hidden sm:block basis-1/4 md:basis-1/10 dark:bg-stone-900 bg-slate-100`} />
     </div>
   );
 };
