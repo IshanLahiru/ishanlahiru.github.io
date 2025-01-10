1004d98991918e419ca8bc182db4521a1598f420
IshanLahiru <ishanlahiru2002@gmail.com>
2025-01-10 22:07:30 +0530

    Enhance NavigationBar with theme toggle and language selection tooltips

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,16 +1,32 @@
-import { Cog6ToothIcon} from '@heroicons/react/20/solid';
+import { LanguageIcon, MoonIcon, SunIcon } from '@heroicons/react/20/solid';
 import React from 'react';
+import { useThemeAndLanguage } from "../../contexts/themeAndLanguage/themeAndLanguageContext";
+import ToolTipWrapper from '../tool-tip-wrapper/toolTipWrapper';
 
 const NavigationBar: React.FC = () => {
+  const { isDarkMode, toggleTheme } = useThemeAndLanguage();
+
   return (
-    <header className="bg-[#08090d] text-white p-4 sm:mt-4 mt-0 sm:rounded-xl rounded-none">
+    <header className="dark:bg-[#08090d] bg-slate-600 text-white p-4 sm:mt-4 mt-0 sm:rounded-xl rounded-none">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-xl font-bold">My Website</h1>
+        <h1 className="text-xl font-bold">ishanlahiru.me</h1>
         <nav>
           <ul className="flex space-x-4">
             <li>
               <a href="#contact" className="flex items-center hover:underline">
-                <Cog6ToothIcon className="h-5 w-5 text-gray-800 dark:text-gray-400 mr-2" />
+              <ToolTipWrapper tooltipText="Change Language" direction="bottom"> 
+                <LanguageIcon className="h-5 w-5 text-gray-300 dark:text-gray-600 mr-2" />
+              </ToolTipWrapper>
+                {isDarkMode ?
+                <ToolTipWrapper tooltipText="Toggle Light" direction="bottom"> 
+                  <MoonIcon className="h-5 w-5 text-gray-300 dark:text-gray-100 mr-2" onClick={toggleTheme} /> 
+                </ToolTipWrapper>
+                :
+                <ToolTipWrapper tooltipText="Toggle Dark" direction="bottom">  
+                  <SunIcon className="h-5 w-5 text-gray-300 dark:text-gray-100 mr-2" onClick={toggleTheme} />
+                </ToolTipWrapper>
+                }
+                
               </a>
             </li>
           </ul>
