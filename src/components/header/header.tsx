5bce4e7d3853b36302c4127e0af84ab7c76eece5
IshanLahiru <ishanlahiru2002@gmail.com>
2025-01-11 06:47:44 +0530

    Update header styles for improved color consistency in light and dark modes

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -7,23 +7,23 @@ const NavigationBar: React.FC = () => {
   const { isDarkMode, toggleTheme } = useThemeAndLanguage();
 
   return (
-    <header className="dark:bg-zinc-800 bg-slate-500 text-white p-4 sm:mt-4 mt-0 sm:rounded-xl rounded-none">
+    <header className="dark:bg-gray-950 bg-gray-200 text-white p-4 sm:mt-4 mt-0 sm:rounded-xl rounded-none">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-xl font-bold">ishanlahiru.me</h1>
+        <h1 className="text-xl font-bold dark:text-slate-200 text-gray-950">ishanlahiru.me</h1>
         <nav>
           <ul className="flex space-x-4">
             <li>
               <a href="#contact" className="flex items-center hover:underline">
               <ToolTipWrapper tooltipText="Change Language" direction="bottom"> 
-                <LanguageIcon className="h-5 w-5 text-gray-300 dark:text-gray-600 mr-2" />
+                <LanguageIcon className="h-5 w-5 dark:text-gray-300 text-gray-600 mr-2" />
               </ToolTipWrapper>
                 {isDarkMode ?
                 <ToolTipWrapper tooltipText="Toggle Light" direction="bottom"> 
-                  <MoonIcon className="h-5 w-5 text-gray-300 dark:text-gray-100 mr-2" onClick={toggleTheme} /> 
+                  <MoonIcon className="h-5 w-5 dark:text-gray-300 text-gray-600 mr-2" onClick={toggleTheme} /> 
                 </ToolTipWrapper>
                 :
                 <ToolTipWrapper tooltipText="Toggle Dark" direction="bottom">  
-                  <SunIcon className="h-5 w-5 text-gray-300 dark:text-gray-100 mr-2" onClick={toggleTheme} />
+                  <SunIcon className="h-5 w-5 dark:text-gray-300 text-gray-600 mr-2" onClick={toggleTheme} />
                 </ToolTipWrapper>
                 }
                 
