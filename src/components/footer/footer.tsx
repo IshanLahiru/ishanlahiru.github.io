cf4222951cfa6fcff447bb8362d4b071aa01c9b0
IshanLahiru <ishanlahiru2002@gmail.com>
2025-01-11 20:46:50 +0530

    Update footer and header components to use neutral background color for improved dark mode styling

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="mb-4 mt-8 rounded-xl bg-gray-200 py-6 dark:bg-gray-950">
+    <footer className="mb-4 mt-8 rounded-xl bg-gray-200 py-6 dark:bg-neutral-900">
       <div className="container mx-auto text-center">
         <p className="text-sm text-gray-600 dark:text-gray-400">
           © {new Date().getFullYear()} Ishan Lahiru Sampath. All rights reserved.
