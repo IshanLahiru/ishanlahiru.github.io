532e90abdfa01b0ebaf1ce0fbfbf058c2354dc3e
IshanLahiru <ishanlahiru2002@gmail.com>
2025-01-11 19:43:16 +0530

    Remove unused import from router component

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from 'react';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import PageNotFound from './pageNotFound';
 import HomePage from './home/homePage';
 
