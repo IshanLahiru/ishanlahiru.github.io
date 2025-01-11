f6ff3aab853c2b43a4edf9e6c681aba3b8b52f3b
IshanLahiru <ishanlahiru2002@gmail.com>
2025-01-11 20:59:46 +0530

    Update ProjectCard and BlogCard components for improved border color in dark mode

--- src/routes/home/homePage.tsx
+++ src/routes/home/homePage.tsx
@@ -68,7 +68,7 @@ const blogs = [
 
 const ProjectCard = ({ title, description, techStack, link }: any) => {
   return (
-    <div className="mb-4 rounded-lg bg-white border border-neutral-900 dark:border-gray-400 p-6 shadow-md dark:bg-neutral-950">
+    <div className="mb-4 rounded-lg bg-white border border-neutral-600 dark:border-gray-400 p-6 shadow-md dark:bg-neutral-950">
       <h3 className="text-lg font-bold text-gray-900 dark:text-gray-100">{title}</h3>
       <p className="my-2 text-sm text-gray-600 dark:text-gray-400">{description}</p>
       <div className="my-2 flex flex-wrap gap-2">
@@ -95,7 +95,7 @@ const ProjectCard = ({ title, description, techStack, link }: any) => {
 
 const BlogCard = ({ title, description, date, link }: any) => {
   return (
-    <div className="mb-4 rounded-lg bg-white border border-neutral-900 dark:border-gray-400 p-6 shadow-md dark:bg-neutral-950">
+    <div className="mb-4 rounded-lg bg-white border border-neutral-600 dark:border-gray-400 p-6 shadow-md dark:bg-neutral-950">
       <h3 className="text-lg font-bold text-gray-900 dark:text-gray-100">{title}</h3>
       <p className="my-2 text-sm text-gray-500 dark:text-gray-400">{date}</p>
       <p className="my-2 text-sm text-gray-600 dark:text-gray-400">{description}</p>
