0b8fd0669e0afc680810300644ea6229ef152e04
IshanLahiru <ishanlahiru2002@gmail.com>
2025-01-11 06:47:36 +0530

    Update BaseContainer styles for improved theme consistency

--- src/components/base-container/baseContainer.tsx
+++ src/components/base-container/baseContainer.tsx
@@ -4,10 +4,10 @@ interface BaseContainerProps {
 
 const BaseContainer: React.FC<BaseContainerProps> = ({ children }) => {
   return (
-    <div className={`flex flex-row h-screen w-screen  dark:bg-stone-900 bg-slate-100`}>
-      <div className={`hidden sm:block basis-1/2 md:basis-1/8 dark:bg-stone-900 bg-slate-100`} />
+    <div className={`flex flex-row h-screen w-screen  dark:bg-neutral-950 bg-neutral-100`}>
+      <div className={`hidden sm:block basis-1/2 md:basis-1/8 dark:bg-neutral-950 bg-neutral-100`} />
       <div className="grow h-screen mx-5">{children}</div>
-      <div className={`hidden sm:block basis-1/2 md:basis-1/8 dark:bg-stone-900 bg-slate-100`} />
+      <div className={`hidden sm:block basis-1/2 md:basis-1/8 dark:bg-neutral-950 bg-neutral-100`} />
     </div>
   );
 };
