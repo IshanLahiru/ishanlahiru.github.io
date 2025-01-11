faaf54e22ebf27561f77031b4eccadcb92e05085
IshanLahiru <ishanlahiru2002@gmail.com>
2025-01-11 19:40:07 +0530

    Update BaseContainer component to use auto height for improved responsiveness

--- src/components/base-container/baseContainer.tsx
+++ src/components/base-container/baseContainer.tsx
@@ -4,11 +4,11 @@ interface BaseContainerProps {
 
 const BaseContainer: React.FC<BaseContainerProps> = ({ children }) => {
   return (
-    <div className={`flex h-screen w-screen flex-row bg-neutral-100 dark:bg-neutral-950`}>
+    <div className={`flex h-auto w-auto flex-row bg-neutral-100 dark:bg-neutral-950`}>
       <div
         className={`md:basis-1/8 hidden basis-1/2 bg-neutral-100 dark:bg-neutral-950 sm:block`}
       />
-      <div className="mx-5 h-screen grow">{children}</div>
+      <div className="mx-5 h-auto grow">{children}</div>
       <div
         className={`md:basis-1/8 hidden basis-1/2 bg-neutral-100 dark:bg-neutral-950 sm:block`}
       />
